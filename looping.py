90d5a7d94041ef222d6dba26dbcca10f093678d8
siva0311-developer <sivaram03112002@gmail.com>
2024-09-06 16:11:22 +0530

    workouts

---
+++ looping.py
@@ -0,0 +1,27 @@
+'''statement'''
+'''a=int(input("enter the number"))
+
+for i in range(a,20,3):
+  print(i)'''
+
+'''range value'''
+
+'''a=int(input("enter the number"))
+
+if(a<10):
+    print("success")
+else:
+    print("Failed")'''
+
+
+'''a=int(input("enter the number"))
+while(a<10):
+    print(a)'''
+
+    
+
+a=1
+while(a<10):
+    a=a+1
+    b=1*10
+    print(a,"x",b,"=",a*b)
