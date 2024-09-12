a5367f41123892d4d8432cf608f5905c6e83b97a
siva0311-developer <sivaram03112002@gmail.com>
2024-09-12 10:34:40 +0530

    upload

---
+++ task11.py
@@ -0,0 +1,33 @@
+a=int(input("Enter The First Value"))
+b=int(input("Enter The Second Value"))
+c=input("Enter The operators")
+if (c=='+'):
+ print(a,"+",b,"=",a+b)
+if(c=='-'):
+  print(a,"-",b,"=",a-b)
+if(c=='*'):
+    print(a,"x",b,"=",a*b)
+if(c=='%'):
+    print(a,"%",b,"=",a%b)    
+
+
+
+
+
+
+
+
+
+'''c=input("Enter The operators")
+if (a+b):
+    print("add")
+elif(a-b):
+    print("sub")
+elif(a*b):
+    print("multi")
+elif(a%b):
+    print("divi")
+else:
+    print("not valid")
+    print(a,"x",b,"=",a*b)'''
+    
